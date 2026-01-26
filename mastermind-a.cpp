700a75e112b2094628142b8f5da04a722ad7a448
jmesbtw <jsu0678@gmail.com>
2026-01-25 22:09:55 -0500

    Added Response Class

--- mastermind-a.cpp
+++ mastermind-a.cpp
@@ -172,3 +172,37 @@ void code::print() const {
     cout << ")";
 }
 
+class response {
+private:
+    int correct;
+    int incorrect;
+
+public:
+    // Constructor
+    response(int corr = 0, int incorr = 0) {
+        correct = corr;
+        incorrect = incorr;
+    }
+
+    // Getters
+    int getCorrect() { return correct; }
+    int getIncorrect() { return incorrect; }
+
+    // Setters
+    void setCorrect(int c) { correct = c; }
+    void setIncorrect(int i) { incorrect = i; }
+};
+
+// Global operator==
+bool operator==(response r1, response r2) {
+    return r1.getCorrect() == r2.getCorrect() && 
+           r1.getIncorrect() == r2.getIncorrect();
+}
+
+// Global operator<<
+ostream& operator<<(ostream& os, response r) {
+    os << "Correct: " << r.getCorrect() 
+       << ", Incorrect: " << r.getIncorrect();
+    return os;
+}
+
