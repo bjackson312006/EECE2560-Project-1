8043e227d8bdeec3896510850a54e6c3d6f923b4
bjackson312006 <blakejackson312006@gmail.com>
2026-01-26 15:02:08 -0500

    split between part a and part b

---
+++ Part-B/Inc/response.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+class response {
+    private:
+        int correct;
+        int incorrect;
+
+    public:
+        // Constructor
+        response(int corr = 0, int incorr = 0);
+
+        // Getters
+        int getCorrect() const;
+        int getIncorrect() const;
+
+        // Setters
+        void setCorrect(int c);
+        void setIncorrect(int i);
+};
+
+// Global operator<<
+std::ostream& operator<<(std::ostream& os, response r);
+
+// Global operator==
+bool operator==(response r1, response r2);
---
+++ Part-B/Src/main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "code.hpp"
+#include "response.hpp"
+#include "mastermind.hpp"
+
+/* main() for part b. */
+int main(void) {
+    mastermind mm;
+    mm.playGame();
+    return 0;
+}
