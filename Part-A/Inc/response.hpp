da9b56b7dc7abb823edc4ca5205961230924a190
VOIDED <bmackmrhs@gmail.com>
2026-01-27 19:16:45 -0500

    Updated to match style rubric

--- Part-A/Inc/response.hpp
+++ Part-A/Inc/response.hpp
@@ -1,25 +1,52 @@
+//
+// Brian Mack, Juan Ipina, Blake Jackson, james Su
+// Project #1 MasterMind-a Upload Link
+// Header for the response class used by the Mastermind game.
+//
 #pragma once
 
-class response {
-    private:
-        int correct;
-        int incorrect;
+#include <iostream>
 
-    public:
-        // Constructor
-        response(int corr = 0, int incorr = 0);
+class response
+{
+private:
+    int correct;
+    int incorrect;
 
-        // Getters
-        int getCorrect() const;
-        int getIncorrect() const;
+public:
+    //
+    // Initializes a response with correct and incorrect counts.
+    // corr is number correct, incorr is number incorrect.
+    //
+    response(int corr = 0, int incorr = 0);
 
-        // Setters
-        void setCorrect(int c);
-        void setIncorrect(int i);
+    //
+    // Returns the number of correct positions.
+    //
+    int getCorrect() const;
+
+    //
+    // Returns the number of incorrect positions.
+    //
+    int getIncorrect() const;
+
+    //
+    // Sets the number of correct positions.
+    //
+    void setCorrect(int c);
+
+    //
+    // Sets the number of incorrect positions.
+    //
+    void setIncorrect(int i);
 };
 
-// Global operator<<
+//
+// Writes a response to the output stream.
+//
 std::ostream& operator<<(std::ostream& os, response r);
 
-// Global operator==
+//
+// Returns true if two responses have equal values.
+//
 bool operator==(response r1, response r2);
--- Part-A/Src/response.cpp
+++ Part-A/Src/response.cpp
@@ -1,29 +1,69 @@
+//
+// Brian Mack, Juan Ipina, Blake Jackson, james Su
+// Project #1 MasterMind-a Upload Link
+// Implementation of the response class for Mastermind.
+//
 #include <iostream>
 #include "response.hpp"
 
 using namespace std;
 
-// Constructor
-response::response(int corr, int incorr) {
+//
+// Initializes a response with correct and incorrect counts
+// corr is number correct, incorr is number incorrect
+//
+response::response(int corr, int incorr)
+{
     correct = corr;
     incorrect = incorr;
 }
 
-// Getters
-int response::getCorrect() const { return correct; }
-int response::getIncorrect() const { return incorrect; }
-
-// Setters
-void response::setCorrect(int c) { correct = c; }
-void response::setIncorrect(int i) { incorrect = i; }
-
-// Global operator==
-bool operator==(response r1, response r2) {
-    return r1.getCorrect() == r2.getCorrect() && r1.getIncorrect() == r2.getIncorrect();
+//
+// Returns the number of correct positions
+//
+int response::getCorrect() const
+{
+    return correct;
 }
 
-// Global operator<<
-ostream& operator<<(ostream& os, response r) {
+//
+// Returns the number of incorrect positions
+//
+int response::getIncorrect() const
+{
+    return incorrect;
+}
+
+//
+// Sets the number of correct positions
+//
+void response::setCorrect(int c)
+{
+    correct = c;
+}
+
+//
+// Sets the number of incorrect positions
+//
+void response::setIncorrect(int i)
+{
+    incorrect = i;
+}
+
+//
+// Returns true if two responses have equal values
+//
+bool operator==(response r1, response r2)
+{
+    return r1.getCorrect() == r2.getCorrect() &&
+           r1.getIncorrect() == r2.getIncorrect();
+}
+
+//
+// Writes a response to the output stream
+//
+ostream& operator<<(ostream& os, response r)
+{
     os << "Correct: " << r.getCorrect() << ", Incorrect: " << r.getIncorrect();
     return os;
 }
